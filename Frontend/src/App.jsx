afec9b2aadb9acf8513828355f68b34f13cc8c3f
bhautik <vachhanib485@gmail.com>
2026-07-03 19:33:10 +0530

    Register page create and create ui done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </div>
   )
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import logo from '../assets/Logo.png'
 import { GoogleLogin } from '@react-oauth/google'
-import Login3d from './../3dImage/Login-3d.png'
+import Register3d from './../3dImage/Register-3d.png'
 
-function Login() {
-    return (
-        <div className="min-h-screen bg-zinc-100 px-4 py-8 flex items-center justify-center sm:px-6 lg:px-8">
+function Register() {
+  return (
+    <div className="min-h-screen bg-zinc-100 px-4 py-8 flex items-center justify-center sm:px-6 lg:px-8">
             <div className="w-full max-w-6xl overflow-hidden rounded-3xl bg-white shadow-2xl ring-1 ring-black/10 lg:grid lg:grid-cols-2">
                 <div className="relative hidden overflow-hidden bg-gradient-to-br from-black via-zinc-900 to-zinc-800 px-10 text-white lg:flex xl:px-16">
                     <div className="relative z-10 flex flex-col justify-start pt-14 xl:pt-16">
                         <h1 className="mt-4 max-w-md text-5xl xl:text-5xl font-normal leading-tight">
-                            Discover fashion that defines your style, all in one place.
+                            Your next favorite outfit is just a click away.
                         </h1>
                         <p className="mt-4 max-w-md text-lg xl:text-lg text-zinc-300 font-light tracking-wide">
-                            Shop permium clothing collections with a seamless shopping experience.
+                            Sign up now and explore the latest fashion trends.
                         </p>
                     </div>
 
                     <div className="pointer-events-none absolute inset-0 bg-[radial-gradient(circle_at_top_left,rgba(255,255,255,0.12),transparent_30%),radial-gradient(circle_at_bottom_right,rgba(255,255,255,0.08),transparent_28%)]" />
                     <img
-                        className="absolute inset-x-0 inset-y-43 bottom-0 z-10 h-[78%] w-full scale-120 object-contain object-bottom px-6 lg:scale-170  xl:px-10 xl:scale-205"
-                        src={Login3d}
+                        className="absolute inset-x-0 inset-y-43 bottom-0 z-10 h-[78%] w-full scale-120 object-contain object-bottom px-6 lg:scale-205 xl:px-10 xl:scale-205 "
+                        src={Register3d}
                         alt="3D Login"
                     />
                 </div>
@@ -30,15 +30,21 @@ function Login() {
                         <img className="h-14 w-14 sm:h-16 sm:w-16" src={logo} alt="StyleHub logo" />
                         <h1 className="mt-1 text-2xl sm:text-3xl font-normal text-slate-900">StyleHub</h1>
                         <h2 className="mt-6 text-3xl sm:text-4xl font-normal tracking-tight text-slate-900">
-                            Welcome Back
+                            Create an Account
                         </h2>
                         <p className="mt-2 text-sm sm:text-base text-zinc-500">
-                            Please login to your account
+                            Please fill in the details to create your account
                         </p>
                     </div>
 
                     <form className="mt-8 flex w-full flex-col gap-4">
                         <input
+                            type="text"
+                            placeholder="Enter Name"
+                            className="w-full rounded-xl border border-zinc-300 px-4 py-3 text-slate-900 outline-none transition placeholder:text-zinc-400 focus:border-black focus:ring-4 focus:ring-zinc-200"
+                        />
+
+                           <input
                             type="text"
                             placeholder="Enter Username"
                             className="w-full rounded-xl border border-zinc-300 px-4 py-3 text-slate-900 outline-none transition placeholder:text-zinc-400 focus:border-black focus:ring-4 focus:ring-zinc-200"
@@ -50,12 +56,8 @@ function Login() {
                             className="w-full rounded-xl border border-zinc-300 px-4 py-3 text-slate-900 outline-none transition placeholder:text-zinc-400 focus:border-black focus:ring-4 focus:ring-zinc-200"
                         />
 
-                        <a href="#" className="text-md underline text-right  text-black">
-                            Forgot your password?
-                        </a>
-
                         <button className="mt-2 w-full cursor-pointer rounded-xl bg-black px-4 py-3 font-semibold text-white transition hover:bg-zinc-800 focus:outline-none focus:ring-4 focus:ring-zinc-300">
-                            Login
+                            Register
                         </button>
                     </form>
 
@@ -67,17 +69,18 @@ function Login() {
 
                     <div className="border-2 border-black rounded-3xl inline-block mb-4">
                         <GoogleLogin
-                            shape="circle" />
+                            shape="circle"
+                            text="signup_with" />
                     </div>
 
 
                     <h3 className="text-md text-center text-zinc-500">
-                        Don't have an account? <a href="#" className="underline text-black">Sign Up</a>
+                        Already have an account? <a href="#" className="underline text-black">Login</a>
                     </h3>
                 </div>
             </div>
         </div>
-    )
+  )
 }
 
-export default Login
+export default Register
